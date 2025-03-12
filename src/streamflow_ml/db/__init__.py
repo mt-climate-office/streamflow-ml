df6ec744fc77dacbb666368e6c96084dfb3ed0ab
colinbrust <colin.brust@gmail.com>
2025-03-12 14:42:52 -0600

    occasionally refresh parquet connection to read in new data.

--- src/streamflow_ml/db/__init__.py
+++ src/streamflow_ml/db/__init__.py
@@ -1,23 +1,33 @@
 import polars as pl
 import geopandas as gpd
+import time
 
 
 class ParquetConn:
     def __init__(self, f):
         self.f = f
-        self.df = pl.scan_parquet(
+        self.schema = {
+            "date": pl.Date,
+            "value": pl.Float64,
+            "model_no": pl.Int32,
+            "location": pl.String,
+            "version": pl.String,
+        }
+        self.last_refresh = 0
+        self.refresh_interval = 15 * 60  # 15 minutes in seconds
+        self.df = self._scan_parquet()
+
+    def _scan_parquet(self):
+        self.last_refresh = time.time()
+        return pl.scan_parquet(
             self.f,
             hive_partitioning=True,
-            schema={
-                "date": pl.Date,
-                "value": pl.Float64,
-                "model_no": pl.Int32,
-                "location": pl.String,
-                "version": pl.String,
-            },
+            schema=self.schema
         )
-
+    
     def __call__(self) -> pl.LazyFrame:
+        if time.time() - self.last_refresh > self.refresh_interval:
+            self.df = self._scan_parquet()
         return self.df
 
 
